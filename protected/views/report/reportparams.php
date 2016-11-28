2051f0311a614eec98e4eab0bf3dbdc9e5470e26
Joshua <joshua.r@aware.co.th>
2016-11-28 09:41:16 +0700

    altered the report parameter for the account list

--- protected/views/report/reportparams.php
+++ protected/views/report/reportparams.php
@@ -71,20 +71,30 @@ if($tagList != ""){
 	}
 }
 
-// $accountList = '';
-// if(Yii::app()->user->name == "ReportsOnlyUser"){
-// 	$accountNumbers = Yii::app()->session['accountNumbers'];
-// 	if(strpos($accountNumbers,'101059') == false){$accountNumbers .= ',101059';}
-// 	$accountList = ' AND code IN ('.$accountNumbers.')';
-// }
-// $sqlList=CHtml::encodeArray(CHtml::listData(Account::model()->findAll(array('condition'=>'companyId='.Yii::app()->user->getState('selectedCompanyId').$accountList,'select'=>'CONCAT(\'AND Account.id=\',id) as  id, CAST(CONCAT(code,\' \',name) AS CHAR CHARACTER SET utf8) as name','order'=>'code')),'id','name'));
-// $sqlList['']='';
-// echo "<pre>";
-// print_r($sqlList);
-// echo "</pre>";
-
-// $sqlList=CHtml::encodeArray(CHtml::listData(Account::model()->findAll(array('condition'=>'companyId='.Yii::app()->user->getState('selectedCompanyId').' AND code = "101059"','select'=>'CONCAT(\'AND Account.id=\',id) as  id, CAST(CONCAT(code,\' \',name) AS CHAR CHARACTER SET utf8) as name','order'=>'code')),'id','name'));
-// $sqlList['']='';
+$accountList = '';
+$accountTags = Yii::app()->user->getState('tagList');
+if($accountTags != ''){
+	$accountPieces = explode(",", $accountTags);
+	if(!in_array('1001', $accountPieces)){
+		$accountPieces[] = '1001';
+		$accountTags = implode(',', $accountPieces);
+	}
+	$account_ids = Yii::app()->db->createCommand(
+		'SELECT DISTINCT aca.accountId as AccountID FROM AccountCustomerAssignment aca                                                    INNER JOIN (SELECT c.id, c.name FROM Customer c WHERE c.code IN ('.$accountTags.')) ww ON aca.customerId = ww.id ORDER BY AccountID')->queryAll();
+		$accountID_string = '';
+		$i = 0;
+		foreach ($account_ids as $acc) {
+			if ($i == 0) {
+				$accountID_string .= $acc['AccountID'];
+			}else{
+				$accountID_string .= ','.$acc['AccountID'];
+			}
+			$i++;
+		}
+		$accountList = ' AND id IN ('.$accountID_string.')';
+	}
+	$sqlList=CHtml::encodeArray(CHtml::listData(Account::model()->findAll(array('condition'=>'companyId='.Yii::app()->user->getState('selectedCompanyId').$accountList,'select'=>'CONCAT(\'AND Account.id=\',id) as  id, CAST(CONCAT(code,\' \',name) AS CHAR CHARACTER SET utf8) as name','order'=>'code')),'id','name'));
+	$sqlList['']='';
 
 ?>
 
